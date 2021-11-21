086f5dc0698d9d338b2ac8531f1d88de7ac020c0
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-21 18:06:37 +0300

    Project was created with spring initializr.

---
+++ src/main/java/com/barisertakus/aibasedhealthapp/AiBasedHealthAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.barisertakus.aibasedhealthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBasedHealthAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiBasedHealthAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barisertakus/aibasedhealthapp/AiBasedHealthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barisertakus.aibasedhealthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBasedHealthAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
