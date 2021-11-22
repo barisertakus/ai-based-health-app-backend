86310e220d866560ce0072e6513e8e1ab9f942a7
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-22 03:44:24 +0300

    Spring auditable has been added for create and update fields.

---
+++ src/main/java/com/barisertakus/aibasedhealthapp/config/PersistenceConfig.java
@@ -0,0 +1,28 @@
+package com.barisertakus.aibasedhealthapp.config;
+
+import com.barisertakus.aibasedhealthapp.security.service.UserDetailsImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class PersistenceConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> {
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+            if(principal instanceof UserDetailsImpl){
+                String username = ((UserDetailsImpl) principal).getUsername();
+                return Optional.of(username);
+            }
+            return Optional.empty();
+        };
+    }
+}
