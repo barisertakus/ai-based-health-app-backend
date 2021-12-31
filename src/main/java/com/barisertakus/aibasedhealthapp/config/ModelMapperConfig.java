596571c6eea4b02d8cbcd65a0cabd6d9778fb214
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-12-31 23:36:06 +0300

    ModelMapperConfig has been updated.

--- src/main/java/com/barisertakus/aibasedhealthapp/config/ModelMapperConfig.java
+++ src/main/java/com/barisertakus/aibasedhealthapp/config/ModelMapperConfig.java
@@ -3,7 +3,9 @@ package com.barisertakus.aibasedhealthapp.config;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ModelMapperConfig {
     @Bean
     public ModelMapper getModelMapper(){
