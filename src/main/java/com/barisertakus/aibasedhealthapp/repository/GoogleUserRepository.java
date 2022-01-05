31840534394f1b5c93869f5cbb7b2c0a7673f4b8
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-05 18:07:17 +0300

    add existsByEmail

--- src/main/java/com/barisertakus/aibasedhealthapp/repository/GoogleUserRepository.java
+++ src/main/java/com/barisertakus/aibasedhealthapp/repository/GoogleUserRepository.java
@@ -3,5 +3,8 @@ package com.barisertakus.aibasedhealthapp.repository;
 import com.barisertakus.aibasedhealthapp.entity.GoogleUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GoogleUserRepository extends JpaRepository<GoogleUser, String> {
+    Boolean existsByEmail(String email);
 }
